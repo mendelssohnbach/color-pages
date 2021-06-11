f6eddcfbd132925d10c81718d3fbe590a9f6dd09
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-06-11 10:52:12 +0900

    update: export Color fun.

--- pages/[color].js
+++ pages/[color].js
@@ -1,5 +1,13 @@
 import colors from '../data/colors.json';
 
+export default function Color({ color }) {
+  return (
+    <div className="color-page" style={{ backgroundColor: color.hex }}>
+      <h1>{color.name}</h1>
+    </div>
+  );
+}
+
 export async function getStaticPaths() {
   const paths = colors.map((color) => {
     return {
